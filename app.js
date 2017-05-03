823492356527868fead31f0874ef28fb3b654b8d
wendongliang <wdl_818@163.com>
2017-05-03 15:02:33 +0800

    使用superagent cheerio 开发爬虫网页

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var utility = require('utility');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
 
 var app = express();
 
-app.get('/index', function (req, res) {
-    var name = req.query.name;
-    var md5name = utility.md5(name);
-    res.send(name)
+app.get('/index', function (req, res, next) {
+    superagent.get('https://cnodejs.org/')
+    .end(function(err, sres){
+        //异常处理
+        if (err) {
+            return next(err);
+        };
+
+        // 拿到网页内容存为$
+        var $ = cheerio.load(sres.text);
+
+        var items = [];
+        $('#topic_list .topic_title').each(function(idx, element){
+            var $element = $(element);
+            items.push({
+                title: $element.attr('title'),
+                href: $element.attr('href')
+            })
+        })
+
+        $('#topic_list .user_avatar img').each(function(idx, element){
+            var $element = $(element);
+
+            for (var i = 0; i < items.length; i++) {
+                items[i].author = $element.attr('title')
+            };
+            
+        })
+
+        res.send(items);
+    })
 })
 
-app.listen(3300, function(){
-    console.log('listen port 3300');
+app.listen(3000, function(){
+    console.log('listen port 3000');
 })
